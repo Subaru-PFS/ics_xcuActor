464dfc35f73c438bd566039e8e6df40c570df33a
Craig Loomis <cloomis@astro.princeton.edu>
2015-03-24 05:01:44 -0400

    Subclass from ICC, to get the Controller logic.

--- python/xcuActor/main.py
+++ python/xcuActor/main.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python
 
-import actorcore.Actor
+import logging
 
-class OurActor(actorcore.Actor.Actor):
-    def __init__(self, name, productName=None, configFile=None, debugLevel=30):
+import actorcore.ICC
+
+class OurActor(actorcore.ICC.ICC):
+    def __init__(self, name, productName=None, configFile=None, debugLevel=logging.INFO):
         # This sets up the connections to/from the hub, the logger, and the twisted reactor.
         #
-        actorcore.Actor.Actor.__init__(self, name, 
-                                       productName=productName, 
-                                       configFile=configFile)
+        actorcore.ICC.ICC.__init__(self, name, 
+                                   productName=productName, 
+                                   configFile=configFile)
 
-        self.configureDevices()
 
-    def configureDevices(self):
-        self.devices = dict()
-
-#
-# To work
 def main():
-    theActor = OurActor('xcu', productName='ics_xcuActor')
+    theActor = OurActor('xcu', productName='xcuActor')
     theActor.run()
 
 if __name__ == '__main__':
