dd8f7c96abbb2f03582b4fc2c09a3196fdd3b600
Craig Loomis <cloomis@astro.princeton.edu>
2015-03-24 05:02:26 -0400

    Add a connect command, to attach controllers.

--- python/xcuActor/Commands/TopCmd.py
+++ python/xcuActor/Commands/TopCmd.py
@@ -18,12 +18,32 @@ class TopCmd(object):
         self.vocab = [
             ('ping', '', self.ping),
             ('status', '', self.status),
-        ]
+            ('connect', '[<controllers>]', self.connect),
+         ]
 
         # Define typed command arguments for the above commands.
         self.keys = keys.KeysDictionary("xcu_xcu", (1, 1),
+                                        keys.Key("controllers", types.String()*(1,None),
+                                                 help='the names of 1 or more controllers to load'),
                                         )
 
+    def connect(self, cmd, doFinish=True):
+        """ Reload all controller objects. """
+
+        if 'controllers' in cmd.cmd.keywords:
+            controllers = cmd.cmd.keywords['controllers'].values
+        else:
+            controllers = eval(self.actor.config.get(self.actor.name, 'controllers'))
+        controllers = map(str, controllers)
+
+        for dev in controllers:
+            try:
+                self.actor.attachController(dev)
+            except Exception as e:
+                cmd.fail('text="failed to connect controller %s: %s"' % (dev, e))
+                return
+        cmd.finish()
+        
 
     def ping(self, cmd):
         """Query the actor for liveness/happiness."""
@@ -36,6 +56,6 @@ class TopCmd(object):
 
         self.actor.sendVersionKey(cmd)
         
-        cmd.inform('text=%s' % (qstr("Present, with devices=%s" % (self.actor.devices.keys()))))
+        cmd.inform('text=%s' % (qstr("Present, with controllers=%s" % (self.actor.controllers.keys()))))
         cmd.finish()
 
