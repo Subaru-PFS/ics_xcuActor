e659a98b454c63e58f74be5f491907c6638072f8
Craig Loomis <cloomis@astro.princeton.edu>
2015-09-11 08:17:18 -0400

    Added --name option to allow multiple actors.

--- python/xcuActor/main.py
+++ python/xcuActor/main.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 
+import argparse
 import logging
 from twisted.internet import reactor
 
 import actorcore.ICC
 
 class OurActor(actorcore.ICC.ICC):
-    def __init__(self, name, productName=None, configFile=None, debugLevel=logging.INFO):
+    def __init__(self, name, productName=None, configFile=None, logLevel=logging.INFO):
         # This sets up the connections to/from the hub, the logger, and the twisted reactor.
         #
         actorcore.ICC.ICC.__init__(self, name, 
                                    productName=productName, 
                                    configFile=configFile)
-
+        self.logger.setLevel(logLevel)
+        
         self.everConnected = False
 
         self.monitors = dict(turbo=0,
@@ -31,7 +33,8 @@ class OurActor(actorcore.ICC.ICC):
     def connectionMade(self):
         if self.everConnected is False:
             logging.info("Attaching all controllers...")
-            # self.attachAllControllers()
+            self.allControllers = self.config.get(self.name, 'startingControllers')
+            self.attachAllControllers()
             self.everConnected = True
 
             # reactor.callLater(10, self.status_check)
@@ -63,7 +66,19 @@ class OurActor(actorcore.ICC.ICC):
                                                                     controller, self.statusLoopCB))
             
 def main():
-    theActor = OurActor('xcu', productName='xcuActor')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', default=None, type=str, nargs='?',
+                        help='configuration file to use')
+    parser.add_argument('--logLevel', default=logging.INFO, type=int, nargs='?',
+                        help='logging level')
+    parser.add_argument('--name', default='xcu', type=str, nargs='?',
+                        help='identity')
+    args = parser.parse_args()
+    
+    theActor = OurActor(args.name,
+                        productName='xcuActor',
+                        configFile=args.config,
+                        logLevel=args.logLevel)
     theActor.run()
 
 if __name__ == '__main__':
