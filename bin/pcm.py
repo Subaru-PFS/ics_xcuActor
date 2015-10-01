1ec3f6475b969f7105748d8eac8d928c55375198
Craig Loomis <cloomis@astro.princeton.edu>
2015-10-01 11:09:43 -0400

    require --cam or --host

--- bin/pcm.py
+++ bin/pcm.py
@@ -18,30 +18,31 @@ def main(argv=None):
                         help='turn on one of the PCM power ports')
     parser.add_argument('--off', type=str, action='append', choices=PCM.PCM.powerPorts, default=[],
                         help='turn off one of the PCM power ports')
-    parser.add_argument('--cam', type=str, action='store', default='',
-                        help='the name name of the PCM board\'s camera')
-    parser.add_argument('--host', type=str, action='store', default='10.1.1.4',
+    parser.add_argument('--host', type=str, action='store', default=None,
                         help='the IP address/name of the PCM board')
+    parser.add_argument('--cam', type=str, action='store', default=None,
+                        help='the name of the PCM board\'s camera')
     parser.add_argument('--debug', action='store_true')
 
     args = parser.parse_args(argv)
     print argv
     print args
 
-    if args.cam:
-        host = 'pcm_%s.pfs' % (args.cam)
-    else:
+    if args.cam is not None:
+        host = 'pcm-%s.pfs' % (args.cam)
+    elif args.host is not None:
         host = args.host
-        
+    else:
+        raise RuntimeError('either --cam or --host must be specified')
+    
     pcm = PCM.PCM(host=host, loglevel=logging.DEBUG if args.debug else logging.INFO)
     
     for p in args.off:
-        print("turning %s off" % (p))
+        print("turning %s %s off" % (host, p))
         pcm.powerOff(p)
     for p in args.on:
-        print("turning %s on" % (p))
+        print("turning %s %s on" % (host, p))
         pcm.powerOn(p)
-
     
 if __name__ == "__main__":
     main()
