aaac561684b814f94c089e9a1d1e7a4748ea6e9a
Craig Loomis <cloomis@astro.princeton.edu>
2018-09-12 00:40:08 -0400

    Add most of the pre-opening checks from the pumping doc.

--- python/xcuActor/Commands/GatevalveCmd.py
+++ python/xcuActor/Commands/GatevalveCmd.py
@@ -20,23 +20,91 @@ class GatevalveCmd(object):
         #
         self.vocab = [
             ('gatevalve', 'status', self.status),
-            ('gatevalve', 'open [@force]', self.open),
+            ('gatevalve', 'open [@(underVacuum)] [@(atAtmosphere)] [@(ok)] [@(reallyforce)]', self.open),
             ('gatevalve', 'close', self.close),
+            ('setLimits', '[<atm>] [<soft>] [<hard>]', self.setLimits),
             ('sam', 'off', self.samOff),
             ('sam', 'on', self.samOn),
         ]
 
         # Define typed command arguments for the above commands.
         self.keys = keys.KeysDictionary("xcu_gatevalve", (1, 1),
+                                        keys.Key("soft", types.Float(),
+                                                 help='soft limit for dPressure'),   
+                                        keys.Key("atm", types.Float(),
+                                                 help='lower limit for atmospheric pressure'),   
+                                        keys.Key("hard", types.Float(),
+                                                 help='hard limit for dPressure'),   
         )
-
-    def status(self, cmd):
+        
+        self.atmThreshold = 460.0  # The absolute lowest air pressure to accept as at atmosphere, Torr
+        self.dPressSoftLimit = 3   # The overridable pressure difference limit for opening, Torr
+        self.dPressHardLimit = 10  # The absolute pressure difference limit for opening, Torr
+        
+    def status(self, cmd, doFinish=True):
         """ Generate all gatevalve keys."""
 
         self.actor.controllers['gatevalve'].status(cmd=cmd)
-        cmd.finish()
+        if doFinish:
+            cmd.finish()
 
-    def _checkOpenable(self, cmd):
+    def setLimits(self, cmd):
+        """(Engineering) set soft and hard dPressures, minimum atmPressure """
+        cmdKeys = cmd.cmd.keywords
+
+        if 'soft' in cmdKeys:
+            self.dPressSoftLimit = cmdKeys['soft'].values[0]
+        if 'hard' in cmdKeys:
+            self.dPressHardLimit = cmdKeys['hard'].values[0]
+        if 'atm' in cmdKeys:
+            self.atmThreshold = cmdKeys['atm'].values[0]
+        cmd.finish(f'text="dPressure limits are atm={self.atmThreshold} soft={self.dPressSoftLimit} hard={self.dPressHardLimit}"')
+        
+    def _checkOpenable(self, cmd, atAtmosphere, dPressLimitFlexible):
+        try:
+            pcm = self.actor.controllers['PCM']
+        except KeyError:
+            return 'PCM controller is not connected'
+        
+        try:
+            pcm.powerOn('interlock')
+        except Exception as e:
+            return f'could not turn on interlock power: {e}'
+        
+        try:
+            dewarPressure = pcm.pressure(cmd=cmd)
+        except Exception as e:
+            return f'could not check cryostat pressure: {e}'
+
+        roughName = self.actor.roughName
+        roughDict = self.actor.models[roughName].keyVarDict
+
+        callVal = self.actor.cmdr.call(actor=roughName, cmdStr="gauge status", timeLim=3)
+        if callVal.didFail:
+            return 'failed to get roughing gauge pressure'
+        
+        callVal = self.actor.cmdr.call(actor=roughName, cmdStr="pump status", timeLim=3)
+        if callVal.didFail:
+            return 'failed to get roughing pump status'
+        
+        # Check what the invalid values are!!!! CPLXXX
+        roughPressure = roughDict['pressure'].getValue()
+        roughSpeed = roughDict['pumpSpeed'].getValue()
+        roughMask, roughErrors = roughDict['pumpErrors'].getValue()
+        
+        if atAtmosphere:
+            if dewarPressure < self.atmThreshold:
+                return f'pressure too low to treat as atmosphere ({dewarPressure} < {self.atmThreshold})'
+
+        dPress = abs(dewarPressure - roughPressure)
+        if dPress >= self.dPressSoftLimit:
+            if dPress >= self.dPressHardLimit:
+                return f'pressure difference ({dPress}) exceeds hard limit ({self.dPressHardLimit})'
+            if dPressLimitFlexible:
+                cmd.warn(f'text="overriding pressure difference soft limit {self.dPressSoftLimit} with {dPress}"')
+            else:
+                return f'pressure difference ({dPress}) exceeds soft limit ({self.dPressSoftLimit})'
+
         return 'unknown pressure differential'
     
     def open(self, cmd):
@@ -45,16 +113,23 @@ class GatevalveCmd(object):
         The hardware interlock might veto the action.
         """
         cmdKeys = cmd.cmd.keywords
-
+        atAtmosphere = 'atAtmosphere' in cmdKeys
+        underVacuum = 'underVacuum' in cmdKeys
+        argCheck = (atAtmosphere is True) ^ (underVacuum is True)
+        if not argCheck:
+            cmd.fail('text="either underVacuum or atAtmosphere must be set"')
+        
         # Actively get rough and dewar side pressures
-        status = self._checkOpenable(cmd)
-
+        status = self._checkOpenable(cmd, atAtmosphere, 'ok' in cmdKeys)
+        
         if status != 'OK':
             if 'force' in cmdKeys:
-                cmd.warn(f'text="gatevalve status is suspect ({status}), but FORCEing it open"')
+                cmd.warn(f'text="gatevalve status is suspect but FORCEing it open ({status})"')
             else:
                 cmd.fail(f'text="gatevalue opening blocked: {status}"')
                 return
+        else:
+            cmd.finish(f'text="gatevalue opening blocked: {status}"')
             
         try:
             self.actor.controllers['gatevalve'].open(cmd=cmd)
